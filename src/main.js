c15c8b3eb5a72d102852f4c8d66cce71a78438e0
Bruce Wayne <muditsjain@gmail.com>
2024-09-16 16:59:52 +0530

    changes in build output

--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
-import core from "./core"
-import Player from "./player"
-import Receiver from "./receiver"
-import MockAdapter from "./adapters/mock"
-import VideoJSAdapter from "./adapters/video"
-import HTML5Adapter from "./adapters/html"
+import core from "./core";
+import Player from "./player";
+import Receiver from "./receiver";
+import MockAdapter from "./adapters/mock";
+import VideoJSAdapter from "./adapters/video";
+import HTML5Adapter from "./adapters/html";
 
 const playerjs = {
-    Player,
-    Receiver,
-    MockAdapter,
-    VideoJSAdapter,
-    HTML5Adapter,
-    CONTEXT: core.CONTEXT,
-    VERSION: core.VERSION,
-    METHODS: core.METHODS,
-    addEvent: core.addEvent
-}
+  Player,
+  Receiver,
+  MockAdapter,
+  VideoJSAdapter,
+  HTML5Adapter,
+  CONTEXT: core.CONTEXT,
+  VERSION: core.VERSION,
+  METHODS: core.METHODS,
+  addEvent: core.addEvent,
+};
 
-if (typeof define === 'function' && define.amd) {
-  define(function () {
-    return playerjs
-  })
-} else {
+// Attach to the window object explicitly
+if (typeof window !== "undefined") {
   window.playerjs = playerjs;
 }
 
+// Export as ES module
 export default playerjs;
