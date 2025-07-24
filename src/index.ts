8e8c92711aa1853fdcf27f2cb6d48d4ceee8a8bd
Aditya Patadia <aditya@gumlet.com>
2025-07-24 18:59:48 +0530

    removed barrel export

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-// Export barrel for easy imports
-export { default as Player } from './player'
-export { default as Receiver } from './receiver'
-export { default as MockAdapter } from './adapters/mock'
-export { default as HTML5Adapter } from './adapters/html'
-export { default as VideoJSAdapter } from './adapters/video'
-export { default as core } from './core'
-export * from './types'
-export { default } from './main'
