16c02c52db8d9f881dbda5cdecb4928475ac5723
Aditya Patadia <aditya@gumlet.com>
2026-03-13 23:22:19 +0530

    missing config added

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 export default defineConfig((options) => ( {
   entry: ['src/main.ts'],
@@ -6,5 +6,8 @@ export default defineConfig((options) => ( {
   sourcemap: true,
   dts: true,
   clean: true,
+  outputOptions: {
+    name: 'playerjs',
+  },
   minify: !options.watch,
 }))
